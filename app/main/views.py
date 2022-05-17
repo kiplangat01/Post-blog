2968d3818b9c470ff5d4ff009dbfd7f2c44f53e6
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-05-17 23:42:06 +0300

    regitering users

--- app/main/views.py
+++ app/main/views.py
@@ -36,7 +36,7 @@ def register():
         user = User(username=form.username.data,email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Your Account Has been Created! You are now able to login  in', 'success')
+        flash('Your Account Has been Created', 'success')
         return redirect(url_for('users.login'))
     return render_template('register.html', title='Register', form=form)
 
